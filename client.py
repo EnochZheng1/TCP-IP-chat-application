b9da693d88b2c90c9adc991008323c836e351b1b
EnochZheng1 <90010396+EnochZheng1@users.noreply.github.com>
2023-11-12 18:23:24 -0600

    simple GUI implementation with Chat History display feature and user name feature and server quitting sequence

--- client.py
+++ client.py
@@ -1,28 +1,72 @@
 import socket
 import threading
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import simpledialog
 
-def receive_messages(sock):
-    while True:
-        try:
-            msg = sock.recv(1024).decode('utf-8')
-            print(msg)
-        except:
-            print("An error occurred!")
-            sock.close()
-            break
-
-# Server IP and port
-SERVER = "127.0.0.1"
+HOST = '127.0.0.1'
 PORT = 12345
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER, PORT))
+class ChatClient:
 
-thread = threading.Thread(target=receive_messages, args=(client,))
-thread.start()
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
 
-while True:
-    message = input('')
-    if message == 'QUIT':
-        break
-    client.send(message.encode('utf-8'))
+        self.window = tk.Tk()
+        self.window.title("Chat Room")
+
+        self.name = simpledialog.askstring("Name", "Enter your name:", parent=self.window)
+        self.sock.sendall(self.name.encode('utf-8'))
+
+        self.chat_area = scrolledtext.ScrolledText(self.window)
+        self.chat_area.pack(padx=20, pady=5)
+        self.chat_area.config(state='disabled')
+
+        self.msg_entry = tk.Entry(self.window, width=50)
+        self.msg_entry.pack(padx=20, pady=5)
+        self.msg_entry.bind("<Return>", self.write)
+
+        self.send_button = tk.Button(self.window, text="Send", command=self.write)
+        self.send_button.pack(padx=20, pady=5)
+
+        self.window.protocol("WM_DELETE_WINDOW", self.on_close)
+
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.start()
+
+        self.window.mainloop()
+
+    def write(self, event=None):
+        message = self.msg_entry.get()
+        if message:
+            self.sock.sendall(message.encode('utf-8'))
+        self.msg_entry.delete(0, tk.END)
+
+    def receive(self):
+        while True:
+            try:
+                message = self.sock.recv(1024).decode('utf-8')
+                if message:
+                    self.chat_area.config(state='normal')
+                    self.chat_area.insert('end', message + '\n')
+                    self.chat_area.yview('end')
+                    self.chat_area.config(state='disabled')
+                else:
+                    # Server has closed the connection
+                    break
+            except OSError:
+                # An error occurred, such as the server being unreachable
+                break
+
+        # Clean up and close the window if disconnected
+        self.chat_area.config(state='normal')
+        self.chat_area.insert('end', 'You have been disconnected from the server.\n')
+        self.chat_area.config(state='disabled')
+        self.sock.close()
+
+    def on_close(self):
+        self.sock.close()
+        self.window.destroy()
+
+client = ChatClient(HOST, PORT)
--- server.py
+++ server.py
@@ -1,27 +1,59 @@
 import socket
 import threading
+import sys
+
+is_server_running = True
+clients = []
+client_threads = []
 
 def handle_client(client_socket):
-    while True:
-        msg = client_socket.recv(1024)
-        if msg:
-            print(f"Received: {msg.decode('utf-8')}")
-            broadcast(msg, client_socket)
+    name = client_socket.recv(1024).decode('utf-8')
+    welcome_message = f"{name} has joined the chat!"
+    broadcast(welcome_message.encode('utf-8'))
 
-def broadcast(message, client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(1024)
+            if msg:
+                broadcast(msg, name)
+            else:
+                # No message means the client has disconnected
+                break
+        except:
+            # An exception occurred, meaning something went wrong (e.g., sudden disconnection)
+            break
+
+    # When the client disconnects
+    client_socket.close()
+    remove(client_socket)
+    goodbye_message = f"{name} has left the chat."
+    broadcast(goodbye_message.encode('utf-8'))
+
+def receive_server_commands():
+    global is_server_running
+    while True:
+        cmd = input("Enter 'quit' to shut down the server: ")
+        if cmd == 'quit':
+            print("Shutting down the server...")
+            is_server_running = False
+            server.close()
+            break
+
+def broadcast(message, name=""):
     for client in clients:
-        if client != client_socket:
-            try:
+        try:
+            if name:
+                client.send(f"{name}: {message.decode('utf-8')}".encode('utf-8'))
+            else:
                 client.send(message)
-            except:
-                client.close()
-                remove(client)
+        except:
+            client.close()
+            remove(client)
 
 def remove(connection):
     if connection in clients:
         clients.remove(connection)
 
-# Server IP and port
 SERVER = "127.0.0.1"
 PORT = 12345
 
@@ -33,9 +65,29 @@ clients = []
 
 print(f"Server listening on {SERVER}:{PORT}...")
 
-while True:
-    client_socket, addr = server.accept()
-    clients.append(client_socket)
-    print(f"Connection from {addr} has been established.")
-    thread = threading.Thread(target=handle_client, args=(client_socket,))
-    thread.start()
+# Start a new thread for server commands
+command_thread = threading.Thread(target=receive_server_commands)
+command_thread.start()
+
+while is_server_running:
+    try:
+        client_socket, addr = server.accept()
+        clients.append(client_socket)
+        print(f"Connection from {addr} has been established.")
+        thread = threading.Thread(target=handle_client, args=(client_socket,))
+        thread.start()
+    except OSError:
+        break
+    
+# Server shutdown sequence
+for client_socket in clients:
+    try:
+        client_socket.sendall("Server is shutting down.".encode('utf-8'))
+    except:
+        pass
+    client_socket.close()
+
+for thread in client_threads:
+    thread.join()
+
+print("Server has been shut down.")
